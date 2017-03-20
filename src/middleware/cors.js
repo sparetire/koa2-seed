8b6e137a10bf14aa5427466cda1db70503db0595
Sparetire <s9ar371r3@gmail.com>
2017-03-20 21:55:24 +0800

    fix: bug fixed

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,8 @@
 function cors(opts) {
-	return async (ctx, next) => {
+	return async(ctx, next) => {
 		ctx.set('Access-Control-Allow-Origin', '*');
-		ctx.set('Access-Control-Request-Method', 'GET,POST,PUT,DELETE,OPTIONS');
-		ctx.set('Access-Control-Request-Headers', 'Content-Type,Content-LengthAccept,Accept-Charset,Accept-Encoding,Authorization,X-Requested-With,Token');
+		ctx.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+		ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Accept,Accept-Charset,Accept-Encoding,Authorization,X-Requested-With,Token');
 		ctx.set('Access-Control-Max-Age', '7200');
 		await next();
 	};
