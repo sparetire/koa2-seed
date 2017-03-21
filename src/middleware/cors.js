9caa2063d35351a30bceec0efceab9f7dd786c8a
Sparetire <s9ar371r3@gmail.com>
2017-03-21 14:25:32 +0800

    fix: bug fixed and update

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -4,6 +4,10 @@ function cors(opts) {
 		ctx.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
 		ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Accept,Accept-Charset,Accept-Encoding,Authorization,X-Requested-With,Token');
 		ctx.set('Access-Control-Max-Age', '7200');
+		if (ctx.request.method === 'OPTIONS') {
+			ctx.body = 'OK';
+			return;
+		}
 		await next();
 	};
 }
